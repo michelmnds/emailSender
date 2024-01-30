0e6281cb4d27ebd92a58faffb08f5afd5dce20f9
michelmnds <m1tchofc@gmail.com>
2024-01-30 22:02:48 +0000

    fix: fixed cors

--- config/index.js
+++ config/index.js
@@ -20,7 +20,11 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ["https://www.apracacfit.com/", "http://localhost:5173"],
+    })
+  );
 
   // In development environment the app logs
   app.use(logger("dev"));
