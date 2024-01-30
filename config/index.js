0d780926038815e73ef15475f5227ed68980f5db
michelmnds <m1tchofc@gmail.com>
2024-01-30 21:57:29 +0000

    fix: fixed cors

--- config/index.js
+++ config/index.js
@@ -13,8 +13,6 @@ const cookieParser = require("cookie-parser");
 // unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN;
-
 // Middleware configuration
 module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
@@ -22,11 +20,7 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: [FRONTEND_URL],
-    })
-  );
+  app.use(cors());
 
   // In development environment the app logs
   app.use(logger("dev"));
