fc7ec2b0723d06294988d37b71d2b15d5ab62c01
michelmnds <m1tchofc@gmail.com>
2024-03-12 11:55:13 +0000

    feat: added condition to 'message'

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 
 router.post("/", async (req, res) => {
   const payload = req.body;
+  let message = null;
 
   const transporter = nodemailer.createTransport({
     service: "Gmail",
@@ -12,12 +13,28 @@ router.post("/", async (req, res) => {
     },
   });
 
-  const message = await transporter.sendMail({
-    from: payload.email,
-    to: process.env.TO_EMAIL,
-    subject: "Um novo formulário foi preenchido - A Praça",
-    text: `Nome: ${payload.nome}, telemóvel: ${payload.telemovel}, objetivo: ${payload.objetivo}, patologias: ${payload.patologia}, personal trainer: ${payload.pt}`,
-  });
+  if (payload.pt) {
+    message = await transporter.sendMail({
+      from: payload.email,
+      to: process.env.TO_EMAIL,
+      subject: "Um novo formulário foi preenchido - A Praça",
+      text: `Nome: ${payload.nome}, telemóvel: ${payload.telemovel}, objetivo: ${payload.objetivo}, patologias: ${payload.patologia}, personal trainer: ${payload.pt}`,
+    });
+  } else if (payload.class) {
+    message = await transporter.sendMail({
+      from: payload.email,
+      to: process.env.TO_EMAIL,
+      subject: "Um novo formulário foi preenchido - A Praça",
+      text: `Nome: ${payload.nome}, telemóvel: ${payload.telemovel}, objetivo: ${payload.objetivo}, aula: ${payload.class}`,
+    });
+  } else if (payload.service) {
+    message = await transporter.sendMail({
+      from: payload.email,
+      to: process.env.TO_EMAIL,
+      subject: "Um novo formulário foi preenchido - A Praça",
+      text: `Nome: ${payload.nome}, telemóvel: ${payload.telemovel}, objetivo: ${payload.objetivo}, serviço: ${payload.service}`,
+    });
+  }
 
   transporter.sendMail(message, (error, info) => {
     if (error) {
